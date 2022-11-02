11c703f1bb162ba69e8630fd09a96bae15708448
VitalijsMuzalovs <102912469+VitalijsMuzalovs@users.noreply.github.com>
2022-11-02 21:05:23 +0200

    web

--- myPHP/test.php
+++ myPHP/test.php
@@ -2,16 +2,8 @@
 
 namespace HexletBasics\Conditionals\IfElse;
 
-function normalizeUrl($url){
-    if (strpos($url,"http") === 0){
-        $pos = strpos($url,'/') ;
-        $url = substr_replace($url,'https://',0,$pos + 2);
-        return $url;
-    }else{
-        return "https://{$url}";
-    }
+function generateAmount($itemCount,$itemPrice){
+    return $itemCount ?: $itemPrice * 3 ;
 }
 
-//$myURL = "www.yandex.ru";
-$myURL = "http://www.yandex.ru";
-print_r(normalizeUrl($myURL));
+print_r(generateAmount(0, 2));
